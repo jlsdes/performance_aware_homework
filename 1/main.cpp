f6daf23eda775da1ab2e07e833dcce33e712f22e
jlsdes <jls.desmet@gmail.com>
2026-03-03 12:06:16 +0100

    Moved some other duplicate code to functors, and addded hexadecimal codes to the table construction for easier searching

--- 1/main.cpp
+++ 1/main.cpp
@@ -153,6 +153,7 @@ std::string get_r_m_name( unsigned char const r_m,
     return stream.str();
 };
 
+
 struct RmToRegDecoder {
     bool force_swap = false;
     bool force_wide = false;
@@ -185,54 +186,62 @@ struct RmToRegDecoder {
 };
 
 
-std::string decode_imm_to_r_m( unsigned char const *& instruction ) {
-    struct Instruction {
-        // First byte
-        unsigned char w : 1;
-        unsigned char s : 1;
-        unsigned char opcode : 6;
-        // Second byte
-        unsigned char r_m : 3;
-        unsigned char subop : 3;
-        unsigned char mod : 2;
-    };
-    auto const values { reinterpret_cast<Instruction const *>( instruction ) };
-    instruction += sizeof( Instruction );
+struct ImmToRmDecoder {
+    bool mid_type = false;
 
-    std::string const destination { get_r_m_name( values->r_m, values->mod, values->w, instruction ) };
-    std::string const source_type { values->w ? "word" : "byte" };
+    std::string operator()( unsigned char const *& instruction ) {
+        struct Instruction {
+            // First byte
+            unsigned char w : 1;
+            unsigned char s : 1;
+            unsigned char opcode : 6;
+            // Second byte
+            unsigned char r_m : 3;
+            unsigned char subop : 3;
+            unsigned char mod : 2;
+        };
+        auto const values { reinterpret_cast<Instruction const *>( instruction ) };
 
-    if ( values->opcode == 0b110001 ) {
-        int const source { get_value( instruction, values->w, values->s ) };
-        return std::format( "{}, {} {}", destination, source_type, source );
-    } else {
+        std::string const mnemonic { get_mnemonic( instruction ) };
+        instruction += sizeof( Instruction );
+        std::string const destination { get_r_m_name( values->r_m, values->mod, values->w, instruction ) };
+        std::string const source_type { values->w ? "word" : "byte" };
+
+        if ( mid_type ) {
+            int const source { get_value( instruction, values->w, values->s ) };
+            return std::format( "{} {}, {} {}", mnemonic, destination, source_type, source );
+        }
         int const source { get_value( instruction, values->s == 0 and values->w == 1, values->s ) };
         if ( values->mod == 0b11 )
-            return std::format( "{}, {}", destination, source );
+            return std::format( "{} {}, {}", mnemonic, destination, source );
         else
-            return std::format( "{} {}, {}", source_type, destination, source );
+            return std::format( "{} {} {}, {}", mnemonic, source_type, destination, source );
     }
-}
+};
 
 
-std::string decode_imm_to_accum( unsigned char const *& instruction ) {
-    struct HeaderByte {
-        unsigned char w : 1;
-        unsigned char d : 1;
-        unsigned char opcode : 6;
-    };
-    auto const header { reinterpret_cast<HeaderByte const *>( instruction ) };
-    instruction += sizeof( HeaderByte );
+struct ImmToAccumDecoder {
+    bool bracketed = false;
 
-    std::string source { std::to_string( get_value( instruction, header->w, true ) ) };
-    if ( header->opcode == 0b101000 )
-        source = '[' + source + ']';
-    std::string destination { header->w ? "ax" : "al" };
-    if ( header->d )
-        std::swap( source, destination );
+    std::string operator()( unsigned char const *& instruction ) {
+        struct HeaderByte {
+            unsigned char w : 1;
+            unsigned char d : 1;
+            unsigned char opcode : 6;
+        };
+        auto const header { reinterpret_cast<HeaderByte const *>( instruction ) };
+        instruction += sizeof( HeaderByte );
 
-    return std::format( "{}, {}", destination, source );
-}
+        std::string source { std::to_string( get_value( instruction, header->w, true ) ) };
+        if ( bracketed )
+            source = '[' + source + ']';
+        std::string destination { header->w ? "ax" : "al" };
+        if ( header->d )
+            std::swap( source, destination );
+
+        return std::format( "{}, {}", destination, source );
+    }
+};
 
 
 std::string decode_only_mnemonic( unsigned char const *& instruction ) {
@@ -261,74 +270,6 @@ std::string move_immediate_reg( unsigned char const *& instruction ) {
 
     return std::format( "mov {}, {}", reg_names[destination], source );
 }
-
-
-std::string move_immediate_r_m( unsigned char const *& instruction ) {
-    struct HeaderByte {
-        unsigned char w : 1;
-        unsigned char opcode : 7;
-    };
-    auto const header { reinterpret_cast<HeaderByte const *>( instruction ) };
-
-    if ( header->opcode != 0b1100011 )
-        return "";
-
-    return std::format( "mov {}", decode_imm_to_r_m( instruction ) );
-}
-
-
-std::string move_accumulator( unsigned char const *& instruction ) {
-    struct HeaderByte {
-        unsigned char w : 1;
-        unsigned char d : 1;
-        unsigned char opcode : 6;
-    };
-    auto const header { reinterpret_cast<HeaderByte const *>( instruction ) };
-
-    if ( header->opcode != 0b101000 )
-        return "";
-
-    return std::format( "mov {}", decode_imm_to_accum( instruction ));
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/// Arithmetic instructions
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-std::string arithmetic_immediate_r_m( unsigned char const *& instruction ) {
-    struct Instruction {
-        // First byte
-        unsigned char w : 1;
-        unsigned char s : 1;
-        unsigned char opcode : 6;
-        // Second byte
-        unsigned char r_m : 3;
-        unsigned char subop : 3;
-        unsigned char mod : 2;
-    };
-    auto const values { reinterpret_cast<Instruction const *>( instruction ) };
-
-    if ( values->opcode != 0b100000 )
-        return "";
-
-    return std::format( "{} {}", get_mnemonic( instruction ), decode_imm_to_r_m( instruction ) );
-}
-
-
-std::string arithmetic_accumulator( unsigned char const *& instruction ) {
-    struct HeaderByte {
-        unsigned char w : 1;
-        unsigned char op2 : 2;
-        unsigned char subop : 3;
-        unsigned char op1 : 2;
-    };
-    auto const header { reinterpret_cast<HeaderByte const *>( instruction ) };
-
-    if ( header->op1 != 0b00 and header->op2 != 0b10 )
-        return "";
-
-    return std::format( "{} {}", get_mnemonic( instruction ), decode_imm_to_accum( instruction ) );
-};
  
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /// Jump instructions
@@ -451,71 +392,71 @@ std::string in_out( unsigned char const *& instruction ) {
 std::array<std::function<std::string(unsigned char const *&)>, 256> constexpr decoding_table() {
     std::array<std::function<std::string(unsigned char const *&)>, 256> table { nullptr };
 
-    // 00___0__
-    // 00___10_
     for ( unsigned char j { 0 }; j < (1 << 3); ++j ) {
         unsigned char const subop ( j << 3 );
+        // 00 01 02 03 08 09 0a 0b 10 11 12 13 18 19 1a 1b 20 21 22 23 28 29 2a 2b 30 31 32 33 38 39 3a 3b
         for ( unsigned char i { 0 }; i < (1 << 2); ++i )
             table[0b0000'0000 | i | subop] = RmToRegDecoder {};
+        // 04 05 0c 0d 14 15 1c 1d 24 25 2c 2d 34 35 3c 3d
         for ( unsigned char i { 0 }; i < (1 << 1); ++i )
-            table[0b0000'0100 | i | subop] = arithmetic_accumulator;
+            table[0b0000'0100 | i | subop] = ImmToAccumDecoder {};
     }
-    // 000__11_
+    // 06 07 0e 0f 16 17 1e 1f
     for ( unsigned char i { 0 }; i < (1 << 3); ++i )
         table[0b0000'0110 | i | (i << 2)] = push_pop_seg_reg;
-    // 001_0111
-    for ( unsigned char i { 0 }; i < (1 << 1); ++i )
-        table[0b0010'0111 | (i << 4)] = decode_only_mnemonic;
-    // 010_____
+    // 27 2f 37 3f
+    for ( unsigned char i { 0 }; i < (1 << 2); ++i )
+        table[0b0010'0111 | (i << 3)] = decode_only_mnemonic;
+    // 40 41 42 43 44 45 46 47 48 49 4a 4b 4c 4d 4e 4f 50 51 52 53 54 55 56 57 58 59 5a 5b 5c 5d 5e 5f
     for ( unsigned char i { 0 }; i < (1 << 5); ++i )
         table[0b0100'0000 | i] = group_reg;
-    // 0111____
+    // 70 71 72 73 74 75 76 77 78 79 7a 7b 7c 7d 7e 7f
     for ( unsigned char i { 0 }; i < (1 << 4); ++i )
         table[0b0111'0000 | i] = jump_conditional;
-    // 100000__
+    // 80 81 82 83
     for ( unsigned char i { 0 }; i < (1 << 2); ++i )
-        table[0b1000'0000 | i] = arithmetic_immediate_r_m;
-    // 1000011_
+        table[0b1000'0000 | i] = ImmToRmDecoder {};
+    // 86 87
     for ( unsigned char i { 0 }; i < (1 << 1); ++i )
         table[0b1000'0110 | i] = RmToRegDecoder {};
-    // 100010__
+    // 88 89 8a 8b
     for ( unsigned char i { 0 }; i < (1 << 2); ++i )
         table[0b1000'1000 | i] = RmToRegDecoder {};
-    // 10001101
+    // 8d
     table[0b1000'1101] = RmToRegDecoder { .force_swap = true };
-    // 10001111
+    // 8f
     table[0b1000'1111] = group_r_m;
-    // 10010___
+    // 90 91 92 93 94 95 96 97
     for ( unsigned char i { 0 }; i < (1 << 3); ++i )
         table[0b1001'0000 | i] = exchange_reg_imm;
-    // 100111__
+    // 9c 9d 9e 9f
     for ( unsigned char i { 0 }; i < (1 << 2); ++i )
         table[0b1001'1100 | i] = decode_only_mnemonic;
-    // 101000__
+    // a0 a1 a2 a3
     for ( unsigned char i { 0 }; i < (1 << 2); ++i )
-        table[0b1010'0000 | i] = move_accumulator;
-    // 1011____
+        table[0b1010'0000 | i] = ImmToAccumDecoder { .bracketed = true };
+    // b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 ba bb bc bd be bf
     for ( unsigned char i { 0 }; i < (1 << 4); ++i )
         table[0b1011'0000 | i] = move_immediate_reg;
-    // 1100010_
+    // c4 c5
     for ( unsigned char i { 0 }; i < (1 << 1); ++i )
         table[0b1100'0100 | i] = RmToRegDecoder { .force_swap = true, .force_wide = true };
-    // 1100011_
+    // c6 c7
     for ( unsigned char i { 0 }; i < (1 << 1); ++i )
-        table[0b1100'0110 | i] = move_immediate_r_m;
-    // 11010111
+        table[0b1100'0110 | i] = ImmToRmDecoder { .mid_type = true };
+    // d7
     table[0b1101'0111] = decode_only_mnemonic;
-    // 111000__
+    // e0 e1 e2 e3
     for ( unsigned char i { 0 }; i < (1 << 2); ++i )
         table[0b1110'0000 | i] = jump_conditional;
-    // 1110_1__
+    // e4 e5 e6 e7 ec ed ee ef
     for ( unsigned char i { 0 }; i < (1 << 2); ++i )
         for ( unsigned char j { 0 }; j < (1 << 1); ++j )
             table[0b1110'0100 | i | (j << 3)] = in_out;
-    // 1111011_
+    // e6 e7
     for ( unsigned char i { 0 }; i < (1 << 1); ++i )
         table[0b1111'0110 | i] = group_r_m;
-    // 1111111_
+    // fe ff
     for ( unsigned char i { 0 }; i < (1 << 1); ++i )
         table[0b1111'1110 | i] = group_r_m;
 
